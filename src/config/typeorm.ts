2cab489848ed3a34f0937664ce8cec71ca883873
kssampson <kylesampsonmusic@gmail.com>
2024-07-29 09:54:44 -0700

    config adjustments for troubleshooting instance connection

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -7,7 +7,7 @@ dotenvConfig();
 const config = {
     type: 'postgres',
     host: `${process.env.DATABASE_HOST}`,
-    port: `${process.env.DATABASE_PORT}`,
+    port: parseInt(`${process.env.DATABASE_PORT}`),
     username: `${process.env.DATABASE_USERNAME}`,
     password: `${process.env.DATABASE_PASSWORD}`,
     database: `${process.env.DATABASE_NAME}`,
@@ -17,7 +17,8 @@ const config = {
     migrations: ["dist/migrations/*{.ts,.js}"],
     autoLoadEntities: true,
     synchronize: false,
-    logging: true
+    logging: true,
+    ssl: process.env.DATABASE_SSL === 'true' ? { rejectUnauthorized: false } : false,
 }
 
 export default registerAs('typeorm', () => config)
